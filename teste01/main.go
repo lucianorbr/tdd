b61b066f8dc4291ff26e321742839a82f276cf84
Luciano Ribeiro <luciano.ribeiro@outlookc.om>
2021-04-14 22:37:55 -0300

    test: " realizacao de teste unitarios."

---
+++ teste01/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+//const prefixoOlaPortugues = "Olá, "
+//
+//func Ola(nome string) string {
+//	if nome == "" {
+//		nome = "Mundo"
+//	}
+//	return prefixoOlaPortugues + nome
+//}
+
+func Ola(nome string, idioma string) string {
+	if nome == "" {
+		nome = "Mundo"
+	}
+
+	return prefixodeSaudacao(idioma) + nome
+}
+
+func prefixodeSaudacao(idioma string) (prefixo string) {
+	switch idioma {
+	case frances:
+		prefixo = prefixoOlaFrances
+	case espanhol:
+		prefixo = prefixoOlaEspanhol
+	default:
+		prefixo = prefixoOlaPortugues
+	}
+	return
+}
+
+func main() {
+	fmt.Println(Ola("Mundo"))
+}
